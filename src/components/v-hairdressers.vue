c3ef8ca730295a52e686fe1fa2f8019d35d32b06
annakurylo <anna.k.2003.g@gmail.com>
2023-10-17 18:57:34 +0300

    change color

--- src/components/v-hairdressers.vue
+++ src/components/v-hairdressers.vue
@@ -28,7 +28,7 @@ export default {
     const tg = window.Telegram.WebApp;
     tg.MainButton.text = "Показати перукарів в чаті";
     tg.MainButton.isVisible = true;
-    tg.MainButton.color = "#000000";
+    tg.MainButton.color = "#ff13a7";
     tg.MainButton.textColor = "#ffffff"
     tg.onEvent("mainButtonClicked", () => {
       tg.sendData("hairderessers");
